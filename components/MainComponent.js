dea09eb372fe55056961be0d21a0602ca366ea27
BANNOURI Achref <bannouri.achref@gmail.com>
2020-09-13 17:00:14 +0200

    Secure Store

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -17,6 +17,7 @@ import { Loading } from './LoadingComponent';
 import { fetchDishes, fetchComments, fetchPromos, fetchLeaders } from '../redux/ActionCreators';
 import Reservation from './ReservationComponent';
 import Favorites from './FavoriteComponent';
+import Login from './LoginComponent';
 
 
 const mapStateToProps = state => {
@@ -76,6 +77,25 @@ const MenuNavigator = createStackNavigator({
     }
   }
 });
+
+const LoginNavigator = createStackNavigator({
+  Login: { screen: Login },
+}, {
+  navigationOptions: ({ navigation }) => ({
+      headerStyle: {
+          backgroundColor: '#512DA8'
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+          color: '#fff'
+      },
+      headerLeft: <Icon name='menu' size={24}
+          color='white'
+          onPress={() => navigation.toggleDrawer()}
+      />
+  })
+});
+
 
 const HomeNavigator = createStackNavigator({
   Home: { screen: Home }
@@ -163,6 +183,21 @@ const CustomDrawerContentComponent = (props) => (
 
 
 const MainNavigator = createDrawerNavigator({
+  Login: {
+    screen: LoginNavigator,
+    navigationOptions: {
+        title: 'Login',
+        drawerLabel: 'Login',
+        drawerIcon: ({ tintColor }) => (
+            <Icon
+                name='sign-in'
+                type='font-awesome'
+                size={24}
+                color={tintColor}
+            />
+        )
+    }
+},
   Home: 
     { screen: HomeNavigator,
       navigationOptions: {
@@ -249,6 +284,7 @@ const MainNavigator = createDrawerNavigator({
 
     
 }, {
+  initialRouteName: 'Home',
   drawerBackgroundColor: '#D1C4E9',
   contentComponent: CustomDrawerContentComponent
 });
